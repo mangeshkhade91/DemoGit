8e244f6a42e502d885bac9561aec6ba64ce3b6dc
HP <HP@Admin>
2022-11-14 23:49:49 +0530

    CLass commit

---
+++ DemoGitProject/src/GitPack/DemoCLass.java
@@ -0,0 +1,12 @@
+package GitPack;
+
+public class DemoCLass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GIT repository");
+
+	}
+
+}
